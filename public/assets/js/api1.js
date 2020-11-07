95027daed8c88543e3eb3fcfe074b9fff5bbab7b
venemexcoin <venemexcoin@gmail.com>
2020-11-07 17:46:37 -0600

    commit actualisacion de 07-11

--- public/assets/js/api1.js
+++ public/assets/js/api1.js
@@ -1,13 +1,2 @@
 console.log('conectado')
 
-const checkbox = document.getElementById('check')
-
-function checkAddress(checkbox) 
-{ 
-    if (checkbox.checked) 
-    { 
-     alert("a"); 
-    }else {
-        alert('b');
-    } 
-} 
